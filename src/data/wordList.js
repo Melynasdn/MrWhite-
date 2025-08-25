c3d2426989182758a71bc95a06081a571e38c417
Melynasdn <melynaferiel.soudani@gmail.com>
2025-08-25 20:12:38 +0100

    WordList updated

--- src/data/wordList.js
+++ src/data/wordList.js
@@ -1,17 +1,21 @@
-
-
 export default {
   easy: [
-    { real: 'pomme', fake: 'poire' },
-    { real: 'chat', fake: 'chien' },
-    { real: 'lit', fake: 'canapé' },
+    { real: 'pomme', fake: 'banane' },
+    { real: 'chat', fake: 'lapin' },
+    { real: 'lit', fake: 'fauteuil' },
+    { real: 'maison', fake: 'appartement' },
+    { real: 'vélo', fake: 'trottinette' },
   ],
   medium: [
-    { real: 'avion', fake: 'hélicoptère' },
-    { real: 'guitare', fake: 'violon' },
+    { real: 'avion', fake: 'drone' },
+    { real: 'guitare', fake: 'ukulele' },
+    { real: 'ordinateur', fake: 'tablette' },
+    { real: 'montagne', fake: 'colline' },
   ],
   hard: [
-    { real: 'algorithme', fake: 'fonction' },
-    { real: 'parallélogramme', fake: 'trapèze' },
+    { real: 'algorithme', fake: 'programme' },
+    { real: 'parallélogramme', fake: 'pentagone' },
+    { real: 'photosynthèse', fake: 'respiration' },
+    { real: 'constellation', fake: 'étoile' },
   ]
 };
